953b9b45ecf630078fe5cdfe45e1a68c73ca09b8
Chris Stein <cstein@bmcc.cuny.edu>
2019-12-14 11:20:44 -0500

    Adding responsive nav.

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var $ = jQuery.noConflict();
+
+
+function hamburger(){
+	$(".hamburger").on('click', function(e){
+		e.preventDefault();
+		if(window.innerWidth<1285){
+			$('nav .mobile-menu').stop(1,0).slideToggle(250,
+				function(){
+					if($(this).css('display') == 'none'){
+						$(this).css('display', '');
+					}
+				});
+		}
+	});
+}
+
+function mobileMenuPos(){
+
+	var h = $('header.site-header').outerHeight();
+
+	$('nav .mobile-menu').css('top', h + 'px');
+
+	if(window.innerWidth>1285){
+		$('nav .mobile-menu').css('display','none');
+	}
+}
+
+jQuery(document).ready(
+	function()
+	{
+		//Project.init();
+
+		//addGray();
+
+		hamburger();
+
+		//homeHover();
+
+		//selecto();
+
+		mobileMenuPos();
+
+		//required();
+
+		$(window).resize(function(){
+			mobileMenuPos();
+
+		})
+	}
+	);
